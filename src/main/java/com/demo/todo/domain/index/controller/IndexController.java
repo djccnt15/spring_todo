73fa597317ee6125e89ee78c0abc6d2c3c176550
Jay.D <djccnt15@gmail.com>
2024-11-07 00:36:31 +0900

    add swagger open-api and create temporal index redirect

---
+++ src/main/java/com/demo/todo/domain/index/controller/IndexController.java
@@ -0,0 +1,26 @@
+package com.demo.todo.domain.index.controller;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping(path = "/")
+public class IndexController {
+    
+    /**
+     * temporal index redirect to swagger-ui
+     * @return redirect
+     */
+    @GetMapping
+    public ResponseEntity<?> redirect() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(URI.create("/swagger-ui/index.html"));
+        return new ResponseEntity<>(headers, HttpStatus.TEMPORARY_REDIRECT);
+    }
+}
