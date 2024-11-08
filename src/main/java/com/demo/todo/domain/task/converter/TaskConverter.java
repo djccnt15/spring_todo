bf26c037df726c18b090659444024c4690a2c8f2
Jay.D <djccnt15@gmail.com>
2024-11-08 22:57:27 +0900

    create TaskConverter

---
+++ src/main/java/com/demo/todo/domain/task/converter/TaskConverter.java
@@ -0,0 +1,21 @@
+package com.demo.todo.domain.task.converter;
+
+import com.demo.todo.annotations.Converter;
+import com.demo.todo.db.entity.TaskEntity;
+import com.demo.todo.domain.task.model.TaskResponse;
+
+@Converter
+public class TaskConverter {
+
+    public TaskResponse toResponse(TaskEntity task) {
+        return TaskResponse.builder()
+            .id(task.getId())
+            .title(task.getTitle())
+            .description(task.getDescription())
+            .status(task.getStatus())
+            .dueDate(task.getDueDate().toString())
+            .createdAt(task.getCreatedAt().toLocalDateTime())
+            .updatedAt(task.getUpdatedAt().toLocalDateTime())
+            .build();
+    }
+}
